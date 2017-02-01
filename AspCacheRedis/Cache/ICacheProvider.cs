53ae4c14440b5fac84c23f712c8da8ff4c40082a
EXT-Chudinov Mikael <ext-Mikael.Chudinov@lindorff.com>
2017-02-01 11:33:13 +0100

    Initial commit.

---
+++ AspCacheRedis/Cache/ICacheProvider.cs
@@ -0,0 +1,57 @@
+﻿namespace AspCacheRedis
+{
+    public interface ICacheProvider
+    {
+        /// <summary>
+        /// Retrieve cached item
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="key">Name of cached item</param>
+        /// <returns>Cached value. Default(T) if item doesn't exist.</returns>
+        T Get<T>(string key);
+
+        /// <summary>
+        /// Insert value into the cache using appropriate name/value pairs
+        /// Use predefined cache duration with absolute expiration
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="value">Item to be cached</param>
+        /// <param name="key">Name of item</param>
+        void Set<T>(string key, T value);
+
+        /// <summary>
+        /// Insert value into the cache using appropriate name/value pairs
+        /// Use predefined cache duration with sliding expiration
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="value">Item to be cached</param>
+        /// <param name="key">Name of item</param>
+        void SetSliding<T>(string key, T value);
+
+        /// <summary>
+        /// Insert value into the cache using
+        /// appropriate name/value pairs WITH a cache duration set in minutes
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="key">Item to be cached</param>
+        /// <param name="value">Name of item</param>
+        /// <param name="duration">Cache duration in minutes with absolute expiration</param>
+        void Set<T>(string key, T value, int duration);
+
+        /// <summary>
+        /// Insert value into the cache using
+        /// appropriate name/value pairs WITH a cache duration set in minutes
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="key">Item to be cached</param>
+        /// <param name="value">Name of item</param>
+        /// <param name="duration">Cache duration in minutes with sliding expiration</param>
+        void SetSliding<T>(string key, T value, int duration);
+
+        /// <summary>
+        /// Remove item from cache
+        /// </summary>
+        /// <param name="key">Name of cached item</param>        
+        void Remove(string key);
+    }
+}
---
+++ AspCacheRedis/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AspCacheRedis
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cache = new HttpCache();
+            cache.Set("key1","Hello!");
+            string str = cache.Get<string>("key1");
+
+            var redis = new CacheManagerCache();
+            redis.Set("key2","Hello2!");
+            string str2 = redis.Get<string>("key2");
+        }
+    }
+}
