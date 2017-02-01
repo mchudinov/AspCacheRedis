54367a448a6cb1ae2d002291d17ac7d7e74a7919
EXT-Chudinov Mikael <ext-Mikael.Chudinov@lindorff.com>
2017-02-01 11:29:16 +0100

    Initial commit.

--- AspCacheRedis/Properties/AssemblyInfo.cs
+++ AspCacheRedis/Properties/AssemblyInfo.cs
@@ -8,13 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AspCacheRedis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("
-
-            ")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspCacheRedis")]
-[assembly: AssemblyCopyright("Copyright © 
-
-             2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
