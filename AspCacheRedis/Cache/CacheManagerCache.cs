855fe1236fa084f3b91962eefc11bacef257b936
Mikael Chudinov <mikael@chudinov.net>
2017-02-09 01:25:09 +0100

    Use real Azure Redis.

---
+++ AspCacheRedis/Cache/CacheManagerCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CacheManager.Core;
+
+namespace AspCacheRedis.Cache
+{
+    public class CacheManagerCache : CacheProvider<ICacheManager<object>>
+    {    
+        protected override ICacheManager<object> InitCache()
+        {
+            var cache = CacheFactory.FromConfiguration<object>("Redis");
+            return cache;
+        }
+
+        public override T Get<T>(string key)
+        {
+            return Cache.Get<T>(KeyPrefix+key);
+        }
+
+        public override void Set<T>(string key, T value) 
+        {
+            Cache.Add(KeyPrefix+key, value);
+            Cache.Expire(KeyPrefix+key, DateTime.Now.AddMinutes(CacheDuration));
+        }
+
+        public override void SetSliding<T>(string key, T value)
+        {
+            Cache.Add(KeyPrefix+key, value);
+            Cache.Expire(KeyPrefix+key, new TimeSpan(0, CacheDuration, 0));
+        }
+
+        public override void Set<T>(string key, T value, int duration)
+        {
+            Cache.Add(KeyPrefix+key, value);
+            Cache.Expire(KeyPrefix+key, DateTime.Now.AddMinutes(duration));
+        }
+
+        public override void SetSliding<T>(string key, T value, int duration)
+        {
+            Cache.Add(KeyPrefix+key, value);
+            Cache.Expire(KeyPrefix+key, new TimeSpan(0, duration, 0));
+        }
+
+        public override void Remove(string key)
+        {
+            Cache.Remove(KeyPrefix+key);
+        }        
+    }
+}
---
+++ AspCacheRedis/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AspCacheRedis.Cache;
+
+namespace AspCacheRedis
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cache = new HttpCache();
+            cache.Set("key1","Hello!");
+            str1.InnerText = cache.Get<string>("key1");
+
+            var redis = new CacheManagerCache();
+            redis.Set("key2","Hello2!");
+            str2.InnerText = redis.Get<string>("key2");
+
+            Session["cache"] = "cache in redis";
+            str3.InnerText = Session["cache"].ToString();
+        }
+    }
+}
